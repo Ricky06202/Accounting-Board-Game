3dedaf7bde5062821cee80dc7b7abb5444e589b9
Azner01 <azneraparicio@hotmail.com>
2022-10-07 23:53:36 -0500

    Dado Numerico
    V1

---
+++ Assets/Codigo/DadoScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DadoScript : MonoBehaviour
+{
+    static Rigidbody rb;
+    public static Vector3 VelocidadDado;
+
+    // Para inicializar
+    void Start(){
+        rb=GetComponent<Rigidbody>();
+    }
+    //Update es llamado un vez por frame
+    void Update(){
+        VelocidadDado=rb.velocity;
+        if (Input.GetKeyDown(KeyCode.Space)){
+            DadonumeroTextScript.numeroDado= 0;
+            float dirX=Random.Range (0,500);
+            float dirY=Random.Range (0,500);
+            float dirZ=Random.Range (0,500);
+            transform.position=new Vector3 (0,2,0);
+            transform.rotation=Quaternion.identity;
+            rb.AddForce(transform.up*500);
+            rb.AddTorque(dirX,dirY,dirZ);
+        }
+    }
+
+}
---
+++ Assets/Codigo/DadonumeroTextScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DadonumeroTextScript : MonoBehaviour
+{
+    Text text;
+    public static int numeroDado;
+
+    // Para inicializar
+    void Start (){
+        text=GetComponent<Text> ();
+    }
+    
+    void Update (){
+        text.text = numeroDado.ToString ();
+    }
+}
